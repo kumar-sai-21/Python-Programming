189c1a1d7cded2c32caf3e7fd3fa9687e6acd4f9
Sai kumar Satapathy <saikumar212000@gmail.com>
2021-08-31 16:18:01 +0530

    Operators

---
+++ Operators/Identity Operators.py
@@ -0,0 +1,6 @@
+list1=[10,20,30]
+list2=[10,20,30]
+a=list1
+print(list1 is list2)  #Here list1 and list2 both are have same values but different object
+print(a is list1)
+print (list1 is not list2)
