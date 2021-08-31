67641af55728c34aae41c16283d8474e6d299797
Sai kumar Satapathy <saikumar212000@gmail.com>
2021-08-31 16:50:53 +0530

    Bitwise & Membership Operators

---
+++ Operators/Bitwise Operator.py
@@ -0,0 +1,14 @@
+# Bitwise AND
+print(10 & 12)
+print(12 & 33)
+
+# Bitwise OR
+print(12 | 10)
+
+# Bitwise Right Shift
+
+print('Bitwise Right Shift 10 >> 1', 10 >> 1)
+
+#  Bitwise Left Shift
+
+print('Bitwise Left Shift 10 << 1', 10 << 1)
---
+++ Operators/Membership Operator.py
@@ -0,0 +1,7 @@
+list1 = [10, 20, 30]
+list2 = [10, 20, 30]
+print(list1 in list2)
+print(list1 == list2)
+print(10 in list1)
+print(10 in list2)
+print(20 in list1)
